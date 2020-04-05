0153ab00c2ba04b045cf6e89ec227907d84a103c
haiming <haiming_liang@hotmail.com>
2020-04-05 16:46:53 +0800

    add one testcase for inserting course table:Haiming

--- src/main/test/com/haiming/test/TestStudentDAO.java
+++ src/main/test/com/haiming/test/TestStudentDAO.java
@@ -1,11 +1,12 @@
 package com.haiming.test;
 
+import com.haiming.dao.*;
 import com.haiming.dao.Student;
-import com.haiming.dao.StudentDynamicSqlSupport;
-import com.haiming.dao.StudentMapper;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.dynamic.sql.insert.render.InsertStatementProvider;
+import org.mybatis.dynamic.sql.render.RenderingStrategies;
 import org.mybatis.dynamic.sql.render.RenderingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -19,6 +20,7 @@ import java.text.SimpleDateFormat;
 import org.mybatis.dynamic.sql.SqlBuilder;
 import com.haiming.dao.StudentMapper;
 import static com.haiming.dao.StudentDynamicSqlSupport.*;
+import   com.haiming.dao.CourseDynamicSqlSupport;
 
 public class TestStudentDAO {
     private SqlSession sqlSession;
@@ -32,7 +34,7 @@ public class TestStudentDAO {
     @Test
     public void testInsertStudentToDB() throws ParseException {
         Student record = new Student();
-        record.setName("梁海明");
+        record.setName("梁海明" + RandomStringUtils.randomAlphabetic(5));
         record.setGender("M");
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         record.setBirthday(simpleDateFormat.parse("1989-08-15"));
@@ -48,8 +50,25 @@ public class TestStudentDAO {
                 .map(birthday).toProperty("birthday")
                 .map(telephone).toProperty("telephone")
                 .build()
-                .render(RenderingStrategy.MYBATIS3);
+                .render(RenderingStrategies.MYBATIS3);
         StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        int rows = mapper.insert(insertStatement);
+        Assert.assertEquals(rows, 1, "Should insert one row into database.");
+    }
+
+    @Test
+    public void insertCouseIntoDB(){
+        Course record = new Course();
+        record.setCoursename("计算机组成原理" + RandomStringUtils.randomAlphabetic(5));
+        record.setTeachername("黄老师");
+        CourseDynamicSqlSupport.Course courseTable = new CourseDynamicSqlSupport.Course();
+        InsertStatementProvider<Course> insertStatement = SqlBuilder.insert(record).into(courseTable)
+                .map(CourseDynamicSqlSupport.id).toProperty("id")
+                .map(CourseDynamicSqlSupport.coursename).toProperty("coursename")
+                .map(CourseDynamicSqlSupport.teachername).toProperty("teachername")
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+        CourseMapper mapper = sqlSession.getMapper(CourseMapper.class);
         int rows = mapper.insert(insertStatement);
         Assert.assertEquals(rows, 1, "Should insert one row into database.");
     }
