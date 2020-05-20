0123e0047437293983d2251f2695eb981918de23
梁海明 <haiming_liang@hotmail.com>
2020-05-20 23:21:08 +0800

    only producer and consumer using the same message converter can work: Haiming

--- src/main/java/com/haiming/services/concrete/CourseMessagingService.java
+++ src/main/java/com/haiming/services/concrete/CourseMessagingService.java
@@ -5,8 +5,10 @@ import com.haiming.dao.Course;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,13 +18,19 @@ public class CourseMessagingService {
     public void setRabbitTemplate(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
+    @Autowired
+    private MessageConverter messageConverter;
 
     private RabbitTemplate rabbitTemplate;
 
     public void sendCourse(Course course){
-        MessageConverter converter = rabbitTemplate.getMessageConverter();
         MessageProperties props = new MessageProperties();
-        Message message = converter.toMessage(course, props);
+        Message message = messageConverter.toMessage(course, props);
         rabbitTemplate.send("amq.direct", "course", message);
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 }
